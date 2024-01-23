c734a93b7e74d60d88ccb538667ce63bd80ee7b8
Kostadin Cvejoski <cvejoski@gmail.com>
2024-01-23 18:02:47 +0100

    fix running the orca metric on linux

--- src/conditional_rate_matching/configs/experiments_configs/crm/crm_experiments_graph.py
+++ src/conditional_rate_matching/configs/experiments_configs/crm/crm_experiments_graph.py
@@ -1,32 +1,25 @@
-from conditional_rate_matching.configs.configs_classes.config_crm import CRMConfig,CRMTrainerConfig
-from conditional_rate_matching.data.graph_dataloaders_config import (
-    EgoConfig,
-    GridConfig,
-    CommunitySmallConfig
-)
+from conditional_rate_matching.configs.configs_classes.config_crm import CRMConfig, CRMTrainerConfig
+from conditional_rate_matching.data.graph_dataloaders_config import CommunitySmallConfig, EgoConfig, GridConfig
 from conditional_rate_matching.data.states_dataloaders_config import StatesDataloaderConfig
 from conditional_rate_matching.models.metrics.metrics_utils import MetricsAvaliable
-from conditional_rate_matching.models.temporal_networks.temporal_networks_utils import (
-    TemporalScoreNetworkAConfig,
-    TemporalMLPConfig
-)
-
+from conditional_rate_matching.models.temporal_networks.temporal_networks_utils import TemporalScoreNetworkAConfig
 
 """
 The following functions create config files for experiments with graph data
 
 """
 
-def experiment_ego(number_of_epochs=300,berlin=True,network="mlp"):
+
+def experiment_ego(number_of_epochs=300, berlin=True, network="mlp"):
     crm_config = CRMConfig()
-    crm_config.data0 = StatesDataloaderConfig(dirichlet_alpha=100.,batch_size=20)
+    crm_config.data0 = StatesDataloaderConfig(dirichlet_alpha=100.0, batch_size=20)
     crm_config.pipeline.number_of_steps = 1000
-    crm_config.trainer = CRMTrainerConfig(number_of_epochs=number_of_epochs,
-                                          windows=berlin,
-                                          metrics=[MetricsAvaliable.mse_histograms,
-                                                   MetricsAvaliable.graphs_plot,
-                                                   MetricsAvaliable.marginal_binary_histograms],
-                                          learning_rate=1e-2)
+    crm_config.trainer = CRMTrainerConfig(
+        number_of_epochs=number_of_epochs,
+        windows=berlin,
+        metrics=[MetricsAvaliable.mse_histograms, MetricsAvaliable.graphs_plot, MetricsAvaliable.marginal_binary_histograms],
+        learning_rate=1e-2,
+    )
 
     if network == "gnn":
         crm_config.data1 = EgoConfig(flatten=False, as_image=False, full_adjacency=True, batch_size=20)
@@ -40,17 +33,18 @@ def experiment_ego(number_of_epochs=300,berlin=True,network="mlp"):
 
     return crm_config
 
-def experiment_comunity_small(number_of_epochs=300,berlin=True,network="mlp"):
+
+def experiment_comunity_small(number_of_epochs=300, berlin=True, network="mlp"):
     crm_config = CRMConfig()
     crm_config.pipeline.number_of_steps = 1000
 
-    crm_config.data0 = StatesDataloaderConfig(dirichlet_alpha=100.,batch_size=20)
-    crm_config.trainer = CRMTrainerConfig(number_of_epochs=number_of_epochs,
-                                          windows=berlin,
-                                          metrics=[MetricsAvaliable.mse_histograms,
-                                                   MetricsAvaliable.graphs_plot,
-                                                   MetricsAvaliable.marginal_binary_histograms],
-                                          learning_rate=1e-2)
+    crm_config.data0 = StatesDataloaderConfig(dirichlet_alpha=100.0, batch_size=20)
+    crm_config.trainer = CRMTrainerConfig(
+        number_of_epochs=number_of_epochs,
+        windows=berlin,
+        metrics=[MetricsAvaliable.mse_histograms, MetricsAvaliable.graphs_plot, MetricsAvaliable.marginal_binary_histograms],
+        learning_rate=1e-2,
+    )
 
     if network == "gnn":
         crm_config.data1 = CommunitySmallConfig(flatten=False, as_image=False, full_adjacency=True, batch_size=20)
@@ -62,20 +56,19 @@ def experiment_comunity_small(number_of_epochs=300,berlin=True,network="mlp"):
         crm_config.temporal_network.time_embed_dim = 100
         crm_config.trainer.learning_rate = 1e-4
 
-
-
     return crm_config
 
-def experiment_grid(number_of_epochs=300,berlin=True,network="mlp"):
-    crm_config = CRMConfig()
-    crm_config.data0 = StatesDataloaderConfig(dirichlet_alpha=100.,batch_size=20)
 
-    crm_config.trainer = CRMTrainerConfig(number_of_epochs=number_of_epochs,
-                                          windows=berlin,
-                                          metrics=[MetricsAvaliable.mse_histograms,
-                                                   MetricsAvaliable.graphs_plot,
-                                                   MetricsAvaliable.marginal_binary_histograms],
-                                          learning_rate=1e-2)
+def experiment_grid(number_of_epochs=300, berlin=True, network="mlp"):
+    crm_config = CRMConfig()
+    crm_config.data0 = StatesDataloaderConfig(dirichlet_alpha=100.0, batch_size=20)
+
+    crm_config.trainer = CRMTrainerConfig(
+        number_of_epochs=number_of_epochs,
+        windows=berlin,
+        metrics=[MetricsAvaliable.mse_histograms, MetricsAvaliable.graphs_plot, MetricsAvaliable.marginal_binary_histograms],
+        learning_rate=1e-2,
+    )
 
     if network == "gnn":
         crm_config.data1 = GridConfig(flatten=False, as_image=False, full_adjacency=True, batch_size=20)
@@ -90,30 +83,26 @@ def experiment_grid(number_of_epochs=300,berlin=True,network="mlp"):
 
     return crm_config
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     from conditional_rate_matching.models.trainers.call_all_trainers import call_trainer
-    from dataclasses import asdict
-    from pprint import pprint
 
-    config = experiment_comunity_small(number_of_epochs=10,network="mlp")
-    #config = experiment_grid(number_of_epochs=10)
-    #config = experiment_ego(number_of_epochs=10,network="gnn")
+    config = experiment_comunity_small(number_of_epochs=10, network="mlp")
+    # config = experiment_grid(number_of_epochs=10)
+    # config = experiment_ego(number_of_epochs=10,network="gnn")
 
-
-    config.trainer.orca_dir = "C:/Users/cesar/Desktop/Projects/DiffusiveGenerativeModelling/Codes/conditional_rate_matching/src/conditional_rate_matching/models/metrics/orca_berlin_2/"
-    config.trainer.windows = True
+    # config.trainer.orca_dir = "C:/Users/cesar/Desktop/Projects/DiffusiveGenerativeModelling/Codes/conditional_rate_matching/src/conditional_rate_matching/models/metrics/orca_berlin_2/"
+    config.trainer.orca_dir = "/home/cvejoski/Projects/conditional_rate_matching/src/conditional_rate_matching/models/metrics/orca"
+    config.trainer.windows = False
 
     config.trainer.save_model_test_stopping = True
     config.trainer.metrics.append(MetricsAvaliable.graphs_metrics)
     config.data1.init = "deg"
 
-    config.thermostat.gamma = 1.
+    config.thermostat.gamma = 1.0
     config.trainer.learning_rate = 1e-3
     config.pipeline.number_of_steps = 10
     config.trainer.loss_regularize_variance = False
 
-    results,metrics = call_trainer(config,
-                                   experiment_name="prenzlauer_experiment",
-                                   experiment_type="crm",
-                                   experiment_indentifier=None)
+    results, metrics = call_trainer(config, experiment_name="prenzlauer_experiment", experiment_type="crm", experiment_indentifier=None)
     print(metrics)
