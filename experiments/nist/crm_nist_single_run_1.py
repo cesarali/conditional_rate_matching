74cbfd94899b5c91c7b753a9da9ccca9b88ae65b
Darius Faroughy <dariusfar@gmail.com>
2024-04-18 17:57:09 -0400

    added arg parser

--- experiments/nist/crm_nist_single_run_1.py
+++ experiments/nist/crm_nist_single_run_1.py
@@ -20,6 +20,8 @@ from conditional_rate_matching.models.metrics.metrics_utils import MetricsAvalia
 from conditional_rate_matching.data.image_dataloader_config import NISTLoaderConfig
 from conditional_rate_matching.data.states_dataloaders_config import StatesDataloaderConfig
 
+from utils import run_nist_analysis
+
 """ Default configurations for training.
 """
 
@@ -44,10 +46,16 @@ def CRM_single_run(dynamics="crm",
                     dropout=0.1,
                     num_layers=3,
                     activation="GELU",
-                    gamma =1.0,
-                    max=1.0,
-                    num_timesteps=50,
-                    ema_decay=0.999
+                    thermostat_params={'gamma': 1.0, 
+                                       'max': 1.0, 
+                                       'slope': 1.0, 
+                                       'shift': 0.65, 
+                                       'exponent': 1.0,
+                                       'log_exponential': 0.1,
+                                       'time_base': 1.0},
+                    num_timesteps=100,
+                    ema_decay=0.999,
+                    run_analysis=True,
                     ):
 
     experiment_files = ExperimentFiles(experiment_name=dynamics,
@@ -110,15 +118,13 @@ def CRM_single_run(dynamics="crm",
         crm_config.data1 = NISTLoaderConfig(flatten=False, as_image=True, batch_size=batch_size, dataset_name=dataset1)
         crm_config.temporal_network = UConvNISTNetConfig()
 
-    if thermostat == "LogThermostat": crm_config.thermostat = LogThermostatConfig(time_exponential=gamma, time_base=1.0,)
-    elif thermostat == "ExponentialThermostat": crm_config.thermostat = ExponentialThermostatConfig(max=max, gamma=gamma,)
-    elif thermostat == "InvertedExponentialThermostat": crm_config.thermostat = InvertedExponentialThermostatConfig(max=max, gamma=gamma,)
-    elif thermostat == "PeriodicThermostat": crm_config.thermostat = PeriodicThermostatConfig(max=max, gamma=gamma,)
-    elif thermostat == "PolynomialThermostat": crm_config.thermostat = PolynomialThermostatConfig(gamma=gamma, exponent=max)
-    elif thermostat == "PlateaulThermostat": crm_config.thermostat = PlateauThermostatConfig(gamma=gamma, slope=max, shift=0.65)
-
-    else: 
-        crm_config.thermostat = ConstantThermostatConfig(gamma=gamma)
+    if thermostat == "LogThermostat": crm_config.thermostat = LogThermostatConfig(time_exponential=thermostat_params['log_exponential'], time_base=thermostat_params['time_base'],)
+    elif thermostat == "ExponentialThermostat": crm_config.thermostat = ExponentialThermostatConfig(max=thermostat_params['max'], gamma=thermostat_params['gamma'],)
+    elif thermostat == "InvertedExponentialThermostat": crm_config.thermostat = InvertedExponentialThermostatConfig(max=thermostat_params['max'], gamma=thermostat_params['gamma'],)
+    elif thermostat == "PeriodicThermostat": crm_config.thermostat = PeriodicThermostatConfig(max=thermostat_params['max'], gamma=thermostat_params['gamma'],)
+    elif thermostat == "PolynomialThermostat": crm_config.thermostat = PolynomialThermostatConfig(gamma=thermostat_params['gamma'], exponent=thermostat_params['exponent'])
+    elif thermostat == "PlateaulThermostat": crm_config.thermostat = PlateauThermostatConfig(gamma=thermostat_params['gamma'], slope=thermostat_params['slope'], shift=thermostat_params['shift'])
+    else: crm_config.thermostat = ConstantThermostatConfig(gamma=thermostat_params['gamma'])
 
     crm_config.trainer = CRMTrainerConfig(number_of_epochs=epochs,
                                           learning_rate=learning_rate,
@@ -135,47 +141,122 @@ def CRM_single_run(dynamics="crm",
     crm = CRMTrainer(crm_config, experiment_files)
     _ , metrics = crm.train()
 
-    print('metrics=',metrics)
+    print('INFO: final metrics = ', metrics)
+
+    if run_analysis:
+       print('INFO: running analysis')
+       run_nist_analysis(experiment_type,
+                        run=experiment_indentifier,
+                        generative_model=dynamics,
+                        num_timesteps=num_timesteps,
+                        time_epsilon=time_embed_dim,
+                        num_img_bridge=6, 
+                        num_intermediate_bridge=5,
+                        device=device)
     return metrics
 
 
 if __name__ == "__main__":
-        
-        import sys
 
-        cuda = sys.argv[1]
-        experiment = sys.argv[2]
-        dim_hidden = int(sys.argv[3])
-        act = sys.argv[4]
-        thermostat = sys.argv[5] + "Thermostat"
-        gamma = sys.argv[6]
-        max = sys.argv[7]
-        dataset0 = experiment.split('_')[0]
+    #..Parse the arguments
 
-        print('experiment=',experiment, 'thermostat=',thermostat, 'gamma=',gamma, 'max=',max, 'dataset0=',dataset0, 'cuda=',cuda)
+    import argparse
+    import random
 
-        if dataset0 == 'noise': dataset0 = None
-        coupling = 'OTPlanSampler' if experiment.split('_')[-1] == 'OT' else 'uniform'
+    parser = argparse.ArgumentParser(description='Run the CRM training with specified configurations.')
+    parser.add_argument('--cuda', type=str, required=True, help='CUDA device number')
+    parser.add_argument('--source', type=str, required=True, help='Source dataset')
+    parser.add_argument('--target', type=str, required=True, help='Target dataset')
+    parser.add_argument('--model', type=str, required=True, help='Model for the network')
+    parser.add_argument('--timesteps', type=int, required=False, help='Number of timesteps', default=100)
+    parser.add_argument('--coupling', type=str, required=False, help='Type of source-target coupling', default='uniform')
+    parser.add_argument('--epochs', type=int, required=False, help='Number of epochs', default=100)
+    parser.add_argument('--dim', type=int, required=False, help='Hidden dimension size', default=145)
+    parser.add_argument('--act', type=str, required=False, help='Activation function', default='Swish')
+    parser.add_argument('--lr', type=float, required=False, help='Learning rate', default=0.0004)
+    parser.add_argument('--ema', type=float, required=False, help='Exponential moving average decay', default=0.9999)
+    parser.add_argument('--thermostat', type=str, required=True, help='Type of thermostat', default='Constant')
+    parser.add_argument('--gamma', type=float, required=True, help='Gamma parameter for thermostat', default=None)
+    parser.add_argument('--max', type=float, required=False, help='Max parameter for thermostat', default=None)
+    parser.add_argument('--slope', type=float, required=False, help='Slope parameter for thermostat', default=None)
+    parser.add_argument('--shift', type=float, required=False, help='Shift parameter for thermostat', default=None)
+    parser.add_argument('--exponent', type=float, required=False, help='Exponent parameter for thermostat', default=None)
+    parser.add_argument('--logexp', type=float, required=False, help='Exponential parameter for thermostat', default=None)
+    parser.add_argument('--timebase', type=float, required=False, help='Time base parameter for thermostat', default=None)
+    args = parser.parse_args()
 
-        CRM_single_run(dynamics="crm",
-               experiment_type=experiment + '_hiddim_' + str(dim_hidden) + '_' + act  +'_' + thermostat + '_gamma_' + gamma + '_max_' + max,
-               model="unet",
-               epochs=100,
-               thermostat=thermostat+"Thermostat",
-               coupling_method=coupling,
-               dataset0=dataset0,
-               dataset1="mnist",
-               metrics = ["mse_histograms", 
-                          'fid_nist', 
-                          "mnist_plot", 
-                          "marginal_binary_histograms"],
-               batch_size=256,
-               learning_rate=0.0004,
-               ema_decay=0.9999,
-               hidden_dim=dim_hidden,
-               time_embed_dim=dim_hidden,
-               gamma=float(gamma),
-               max=float(max),
-               activation=act,
-               num_timesteps=200,
-               device="cuda:" + cuda)
+    params = ['gamma', 'max', 'slope', 'shift', 'exponent', 'logexp', 'timebase']
+    therm_params = {}
+    for p in params:
+        if p in vars(args).keys():
+            if vars(args)[p] is not None:
+                therm_params[p] = vars(args)[p]
+
+    # Determine dataset0 based on experiment name
+    import datetime
+    date = datetime.datetime.now().strftime("%Y.%m.%d_%H:%M:%S")
+    full_experiment_type = f"{args.source}_to_{args.target}_{args.model}_dim_{args.dim}_{args.act}_{args.thermostat}Thermostat" + "_" + "_".join([f"{k}_{v}" for k,v in therm_params.items()]) + "__" + date 
+
+
+    # Call the CRM_single_run function with the parsed arguments
+    CRM_single_run(dynamics="crm",
+                   experiment_type=full_experiment_type,
+                   model=args.model,
+                   epochs=args.epochs,
+                   thermostat=args.thermostat + "Thermostat",
+                   coupling_method=args.coupling,
+                   dataset0=None if args.source == 'noise' else args.source,
+                   dataset1=args.target,
+                   metrics=["mse_histograms", 
+                            "fid_nist", 
+                            "mnist_plot",
+                            "marginal_binary_histograms"],
+                   batch_size=256,
+                   learning_rate=args.lr,
+                   ema_decay=args.ema,
+                   hidden_dim=args.dim,
+                   time_embed_dim=args.dim,
+                   thermostat_params=therm_params,
+                   activation=args.act,
+                   num_timesteps=args.timesteps,
+                   device="cuda:" + args.cuda)
+
+
+    #     # import sys
+
+    #     # cuda = sys.argv[1]
+    #     # experiment = sys.argv[2]
+    #     # dim_hidden = int(sys.argv[3])
+    #     # act = sys.argv[4]
+    #     # thermostat = sys.argv[5] + "Thermostat"
+    #     # gamma = sys.argv[6]
+    #     # max = sys.argv[7]
+    #     # dataset0 = experiment.split('_')[0]
+
+    #     # print('experiment=',experiment, 'thermostat=',thermostat, 'gamma=',gamma, 'max=',max, 'dataset0=',dataset0, 'cuda=',cuda)
+
+    #     # if dataset0 == 'noise': dataset0 = None
+    #     # coupling = 'OTPlanSampler' if experiment.split('_')[-1] == 'OT' else 'uniform'
+
+    #     # CRM_single_run(dynamics="crm",
+    #     #        experiment_type=experiment + '_hiddim_' + str(dim_hidden) + '_' + act  +'_' + thermostat + '_gamma_' + gamma + '_max_' + max,
+    #     #        model="unet",
+    #     #        epochs=2,
+    #     #        thermostat=thermostat+"Thermostat",
+    #     #        coupling_method=coupling,
+    #     #        dataset0=dataset0,
+    #     #        dataset1="mnist",
+    #     #        metrics = ["mse_histograms", 
+    #     #                   'fid_nist', 
+    #     #                   "mnist_plot", 
+    #     #                   "marginal_binary_histograms"],
+    #     #        batch_size=256,
+    #     #        learning_rate=0.0004,
+    #     #        ema_decay=0.9999,
+    #     #        hidden_dim=dim_hidden,
+    #     #        time_embed_dim=dim_hidden,
+    #     #        gamma=float(gamma),
+    #     #        max=float(max),
+    #     #        activation=act,
+    #     #        num_timesteps=10,
+    #     #        device="cuda:" + cuda)
