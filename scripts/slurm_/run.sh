0fa1ccc48b284c4a553b2643ab75dbc6a262b3d7
Darius Faroughy <df630@amarel1.amarel.rutgers.edu>
2024-05-10 16:18:12 -0400

    update

--- scripts/slurm_/run.sh
+++ scripts/slurm_/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=gpu              # Partition (job queue)
 #SBATCH --requeue                    # Return job to the queue if preempted
-#SBATCH --job-name=NLL               # Assign a short name to your job
+#SBATCH --job-name=mnist             # Assign a short name to your job
 #SBATCH --nodes=1                    # Number of nodes you require
 #SBATCH --ntasks=1                   # Total number of tasks across all nodes
 #SBATCH --cpus-per-task=1            # Cores per task
-#SBATCH --mem=16G                    # Real memory required
-#SBATCH --time=3:00:00               # Total run time limit
+#SBATCH --mem=10G                    # Real memory required
+#SBATCH --time=6:00:00              # Total run time limit
 #SBATCH --gres=gpu:1                                            # Number of GPUs
 #SBATCH --exclude=gpu009,gpu[005-006],cuda[001-008],volta[001-003]     # exclude specific GPUs
 #SBATCH --output=./log/%x_%N_jobid_%j.out                       # STDOUT output file
@@ -14,9 +14,16 @@
 
 source ~/.bashrc
 conda activate conditional_rate_matching
-cd ../../experiments/nist/loglikelihood/
+cd ../../experiments/nist
 
 echo "Initial GPU Status:"
 nvidia-smi
 
-python3 loglikelihood.py --fwd_process "$1" --bck_process "$2" --timesteps $3
+python3 crm_nist_single_run_1.py --source "$1" \
+                                 --target "$2" \
+                                 --model "$3" \
+                                 --coupling "$4" \
+                                 --thermostat "$5" \
+                                 --gamma $6 \
+                                 --epochs $7 \
+                                 --timesteps $8
