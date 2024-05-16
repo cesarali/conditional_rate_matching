167f9c7ed9aeb67955e33627d6a19b06582197f6
Darius Faroughy <df630@amarel2.amarel.rutgers.edu>
2024-05-16 13:13:45 -0400

    update

--- scripts/slurm_/run_cifar.sh
+++ scripts/slurm_/run_cifar.sh
@@ -5,9 +5,10 @@
 #SBATCH --nodes=1                    # Number of nodes you require
 #SBATCH --ntasks=1                   # Total number of tasks across all nodes
 #SBATCH --cpus-per-task=1            # Cores per task
-#SBATCH --mem=40G                    # Real memory required
-#SBATCH --time=24:00:00              # Total run time limit
-#SBATCH --gres=gpu:1                                            # Number of GPUs
+#SBATCH --mem=10G                    # Real memory required
+#SBATCH --time=72:00:00              # Total run time limit
+#SBATCH --gres=gpu:1             
+#SBTACH --nodelist=gpu027,gpu26                               # Number of GPUs
 #SBATCH --exclude=gpu009,gpu[005-006],cuda[001-008],volta[001-003]     # exclude specific GPUs
 #SBATCH --output=./log/%x_%N_jobid_%j.out                       # STDOUT output file
 #SBATCH --error=./log/%x_%N_jobid_%j.err                        # STDERR output file
@@ -15,5 +16,8 @@
 source ~/.bashrc
 conda activate conditional_rate_matching
 cd ../../src/conditional_rate_matching/configs/experiments_configs/crm
+
+echo "Initial GPU Status:"
+nvidia-smi
 
 python3 crm_experiments_Cifar.py 
